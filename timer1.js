f12a01c0b26e583ca209a1b06a3671bab46714f4
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-07-28 22:39:09 +0000

    some changes are made ot make it legit

--- timer1.js
+++ timer1.js
@@ -2,8 +2,10 @@ let array = process.argv.slice(2);
 array = array.sort((a, b) => a - b);  // 1 3 5
 
 for (let each of array) {
+  each =Number(each);
   setTimeout(()=>{
-    process.stdout.write('\x07');
-  },each)
+    console.log(('\x07'));
+  },each*1000)
 }
 
+console.log('\u0007');
